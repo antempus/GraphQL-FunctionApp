8e20adaab5d1f24896bcaa99d57501e1cd4b7171
Robert Steele <rosteele@starbucks.com>
2020-04-18 09:06:08 -0700

    add logging

--- HttpFunction/index.js
+++ HttpFunction/index.js
@@ -1,4 +1,8 @@
 module.exports = async function (context, req) {
+
+    context.log('this is a context log');
+    console.log('this is a console log');
+
         context.res = {
             // status: 200, /* Defaults to 200 */
             body: "how long did this take???"
