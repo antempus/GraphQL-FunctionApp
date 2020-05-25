265c84f3f01466e39480f9292695c5a867dddcf0
Robert Steele <stryakr@gmail.com>
2020-05-25 08:40:05 -0700

    changes

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,64 +1,46 @@
-import { CosmosClient } from "@azure/cosmos";
-import IUser from "../lib/User";
+import {CosmosClient} from '@azure/cosmos';
+import IUser from '../lib/User';
 
 export const userClient = new CosmosClient(process.env.CosmosKey)
-  .database("database")
-  .container("users");
+                              .database('database')
+                              .container('users');
 
 const removeDBMetadata = (resource) => {
-  const { _ts, _rid, _etag, _attachments, ...rest } = resource;
+  const {_ts, _rid, _etag, _attachments, ...rest} = resource;
   return rest;
 };
-async function getUser(
-  _,
-  {
-    userId,
-  }: {
-    userId: string;
-  }
-) {
-  return userClient
-    .item(userId, userId)
-    .read()
-    .then(({ resource }) => removeDBMetadata(resource))
-    .catch((error) => {
-      console.log(error);
-    });
+async function getUser(_, {
+  userId,
+}: {userId: string;}) {
+  return userClient.item(userId, userId)
+      .read()
+      .then(({resource}) => removeDBMetadata(resource))
+      .catch((error) => {
+        console.log(error);
+      });
 }
-async function addUser(
-  _,
-  {
-    user,
-  }: {
-    user: IUser;
-  }
-) {
-  return userClient.items
-    .upsert(user)
-    .then(({ resource }) => removeDBMetadata(resource))
-    .catch((error) => {
-      console.log(error);
-    });
+async function addUser(_, {
+  user,
+}: {user: IUser;}) {
+  return userClient.items.upsert(user)
+      .then(({resource}) => removeDBMetadata(resource))
+      .catch((error) => {
+        console.log(error);
+      });
 }
-async function addUsers(
-  _,
-  {
-    users,
-  }: {
-    users: [IUser];
-  }
-) {
-  return userClient.items
-    .upsert(users)
-    .then(() => users.length)
-    .catch((error) => {
-      console.log(error);
-    });
+async function addUsers(_, {
+  users,
+}: {users: [IUser];}) {
+  return userClient.items.upsert(users)
+      .then(() => users.length)
+      .catch((error) => {
+        console.log(error);
+      });
 }
 
 const resolvers = {
-  Query: { getUser },
-  Mutation: { addUser, addUsers },
+  Query: {getUser},
+  Mutation: {addUser, addUsers},
 };
 
 export default resolvers;
